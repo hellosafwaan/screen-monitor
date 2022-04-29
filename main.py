96e42a7e24f71fb1652ef7ab46378e766913ce23
Safwaan <hellosafwaan@gmail.com>
2022-04-29 22:05:30 +0530

    refactor - modified  check_subFolder_exits function to check_folder_exits

--- main.py
+++ main.py
@@ -48,10 +48,11 @@ def create_folder(new_folder_name, parent_folder_id = None, should_return_detail
     if should_return_details:
         return get_file_details(new_folder_name,'application/vnd.google-apps.folder', parentFileId = parent_folder_id)
 
-def check_subFolder_exits(parent_folder_id, sub_folder_name):
-
-    sub_folder = DRIVE.files().list(q = "parents = '{}' and name = '{}' and  mimeType = 'application/vnd.google-apps.folder'".format(parent_folder_id, sub_folder_name)).execute().get('files')
-    
+def check_folder_exits( sub_folder_name, parent_folder_id = None):
+    if parent_folder_id == None:
+        sub_folder = DRIVE.files().list(q = "name = '{}' and  mimeType = 'application/vnd.google-apps.folder'".format(sub_folder_name)).execute().get('files')
+    else:
+        sub_folder = DRIVE.files().list(q = "parents = '{}' and name = '{}' and  mimeType = 'application/vnd.google-apps.folder'".format(parent_folder_id, sub_folder_name)).execute().get('files')    
     if len(sub_folder) == 1:
         return True
     elif len(sub_folder) > 1:
